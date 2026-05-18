1a84394bd0c6361d97851b58600d6b25bbcef916
trihdiep1234-art <trihdiep1234@gmail.com>
2026-05-18 23:17:09 +0700

    Add streamlit frontend

---
+++ scripts/video_reader.py
@@ -0,0 +1,44 @@
+import cv2
+import pandas as pd
+video_path = "data/video/sample.mp4"
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+print(frame.shape)
+cv2.imwrite(
+    "outputs/images/frame0.jpg",
+    frame
+)
+csv_path = "data/csv/mock_partition.csv"
+df = pd.read_csv(csv_path)
+# OVERLAY PARTITIONS
+
+for index, row in df.iterrows():
+    x = int(row["x"])
+    y = int(row["y"])
+    w = int(row["w"])
+    h = int(row["h"])
+
+    depth = int(row["depth"])
+    if depth == 0:
+        color = (0, 255, 0)
+
+    elif depth == 1:
+        color = (255, 0, 0)
+
+    elif depth == 2:
+        color = (0, 0, 255)
+
+    else:
+        color = (0, 255, 255)
+    cv2.rectangle(
+        frame,
+        (x, y),
+        (x + w, y + h),
+        color,
+        2
+    )
+output_path = "outputs/images/partition_overlay.jpg"
+cv2.imwrite(output_path, frame)
+print("Overlay image saved")
+print("Saved at:", output_path)
+cap.release()
